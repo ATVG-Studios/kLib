0aec49a291ba8f469b9bb6af27c102306903b7df
AtjonTV <thomas.obernosterer@atvg-studios.com>
2019-09-28 21:45:14 +0200

    functions: Dotnet like 'using' function

---
+++ src/main/kotlin/klib/functions/Using.kt
@@ -0,0 +1,17 @@
+package klib.functions
+
+import java.io.Closeable
+
+/**
+ *  .Net like function that proxies Kotlin's .use
+ *
+ *  @param receiver The Closable input
+ *  @param block The function that uses receiver
+ *  @return Some data from block
+ *
+ * @since 4.0.0
+ * @author Thomas Obernosterer
+ */
+inline fun <T: Closeable?, R> using(receiver: T, block: (T) -> R): R {
+    return receiver.use(block)
+}
