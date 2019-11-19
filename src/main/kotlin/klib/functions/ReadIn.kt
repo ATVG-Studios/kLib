79aa988b3e2ef1ffe9d065c7939bc63c262c53bb
AtjonTV <thomas.obernosterer@atvg-studios.com>
2019-11-19 12:02:36 +0100

    Other: Place version over author

--- src/main/kotlin/klib/functions/ReadIn.kt
+++ src/main/kotlin/klib/functions/ReadIn.kt
@@ -6,8 +6,8 @@ package klib.functions
  * @param default The default when input was null (Default: -1)
  * @return User input as Integer or default
  *
- * @author Thomas Obernosterer
  * @since 4.0.0
+ * @author Thomas Obernosterer
  */
 fun readInt(default: String = "-1"): Int {
     val input = readLine() ?: default
@@ -20,8 +20,8 @@ fun readInt(default: String = "-1"): Int {
  * @param default The default when input was null (Default: -1)
  * @return User input as Long or default
  *
- * @author Thomas Obernosterer
  * @since 4.0.0
+ * @author Thomas Obernosterer
  */
 fun readLong(default: String = "-1"): Long {
     val input = readLine() ?: default
