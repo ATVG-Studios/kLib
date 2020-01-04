59de98231f35b799a8825f514f4d82f7b982fced
AtjonTV <thomas.obernosterer@atvg-studios.com>
2020-01-04 14:43:30 +0100

    Test: Konfig parsing test

---
+++ src/test/kotlin/devtests/Kofing.kt
@@ -0,0 +1,17 @@
+package devtests
+
+import klib.Konfig
+
+fun main() {
+    val testKonfig = """
+        test=["I","need","HELP!"]
+        help=false
+        map={a:0,b:102}
+    """.trimIndent()
+
+    println(Konfig.parseString(testKonfig))
+
+    if((Konfig.parseString(testKonfig)["help"] as Boolean)) {
+        println("Help was True")
+    }
+}
