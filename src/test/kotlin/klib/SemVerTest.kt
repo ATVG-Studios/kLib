b7e766a9cab0b7ba60ba6487a31c616de837ba3a
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2020-06-03 12:52:17 +0200

    Test: Add SemVer test

---
+++ src/test/kotlin/klib/SemVerTest.kt
@@ -0,0 +1,23 @@
+package klib
+
+import klib.exceptions.InvalidTypeException
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+import org.junit.Test
+
+class SemVerTest {
+    @Test
+    fun `SemVer is able to parse a valid Version string`() {
+        val sv = SemVer.parse("1.3.4-rc+b100")
+        assertTrue {
+            sv.major == 1 && sv.minor == 3 && sv.patch == 4 && sv.preRelease == "rc" && sv.buildMetadata == "b100"
+        }
+    }
+
+    @Test
+    fun `SemVer throws a InvalidTypeException when trying to parse an invalid Version string`() {
+        assertFailsWith<InvalidTypeException> {
+            SemVer.parse("1.3.4_-rc+b100")
+        }
+    }
+}
