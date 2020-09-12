be51d665c88f2358035a1e04b208c63695876701
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2020-09-12 16:50:11 +0200

    os: Implement Platform information API

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ and this project adheres to [Semantic Versioning](semver.md).
 - `mod97` extension for String
 - isVirtual parameter to ZipFile constructor
 - ZipFile.openVirtual to output the file to a random Stream
+- `klib.os.Platform` object
 ### Changed
 - Upgraded Kotlin from 1.3.71 to 1.4.0
 - FFDB v1 files are read-only. (FFDB is still Experimental so this change is acceptable)
---
+++ src/main/kotlin/klib/os/Platform.kt
@@ -0,0 +1,67 @@
+package klib.os
+
+import java.security.AccessController
+import java.security.PrivilegedAction
+
+/**
+ * Simple accessor to OS Information
+ *
+ * @since 5.1.0
+ * @author Thomas Obernosterer
+ */
+object Platform {
+    private val os = System.getProperty("os.name")
+    private val javaFxPlatform = AccessController.doPrivileged(PrivilegedAction {
+        System.getProperty(
+            "javafx.platform"
+        )
+    } as PrivilegedAction<String>)
+
+    /**
+     * Check if the current OS is Android
+     *
+     * @since 5.1.0
+     * @author Thomas Obernosterer
+     */
+    val IsAndroid = javaFxPlatform == "android" || System.getProperty("java.vm.name") == "Dalvik"
+
+    /**
+     * Check if the current OS is Windows
+     *
+     * @since 5.1.0
+     * @author Thomas Obernosterer
+     */
+    val IsWindows = os.startsWith("Windows")
+
+    /**
+     * Check if the current OS is macOS
+     *
+     * @since 5.1.0
+     * @author Thomas Obernosterer
+     */
+    val IsMac = os.startsWith("Mac")
+
+    /**
+     * Check if the current OS is Linux but not Android
+     *
+     * @since 5.1.0
+     * @author Thomas Obernosterer
+     */
+    val IsLinux = os.startsWith("Linux") && !IsAndroid
+
+    /**
+     * Check if the current OS is Solaris
+     *
+     * @since 5.1.0
+     * @author Thomas Obernosterer
+     */
+    val IsSolaris = os.startsWith("SunOS")
+
+    /**
+     * Check if the current OS is iOS
+     *
+     * @since 5.1.0
+     * @author Thomas Obernosterer
+     */
+    val IsiOS = os.startsWith("iOS")
+}
