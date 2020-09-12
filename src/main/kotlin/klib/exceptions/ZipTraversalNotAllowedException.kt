23e73f6ba4460bd8ca414cc66955131b996d0c14
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2020-09-12 16:56:54 +0200

    Docs: Add JavaDoc on ZipTraversalNotAllowedException

--- src/main/kotlin/klib/exceptions/ZipTraversalNotAllowedException.kt
+++ src/main/kotlin/klib/exceptions/ZipTraversalNotAllowedException.kt
@@ -2,4 +2,12 @@ package klib.exceptions
 
 import java.lang.Exception
 
+/**
+ * Custom exception
+ *
+ * @param filePath
+ *
+ * @since 5.1.0
+ * @author Thomas Obernosterer
+ */
 class ZipTraversalNotAllowedException(filePath: String) : Exception("ZipFile found a path that seems to try to traverse out of the Zip Package: $filePath")
