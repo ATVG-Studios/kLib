72e4ea8651655ff680d6e9a34c6a7396c1de507d
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2020-09-30 09:24:01 +0200

    extensions: Map.mergeArrays and Map.smartMerge now have a conflict resolver
    * By default the non-null value for a doublicate key will be selected

--- src/main/kotlin/klib/extensions/MutableMap.kt
+++ src/main/kotlin/klib/extensions/MutableMap.kt
@@ -1,17 +1,22 @@
 package klib.extensions
 
+import klib.exceptions.IncompatibleArrayLengthException
+
 /**
  * Merge a Map into a MutableMap without overwriting existing keys
  *
  * @param other The map to merge
+ * @param onConflict Lambda to express how conflicts are resolved
  *
  * @since 3.0.0
  * @author Thomas Obernosterer
  */
-fun <K, V> MutableMap<K, V>.smartMerge(other: Map<K, V>) {
+fun <K, V> MutableMap<K, V>.smartMerge(other: Map<K, V>, onConflict: (V?, V) -> V = { x, y -> x ?: y }) {
     other.forEach { (key, value) ->
         if (key !in this) {
             this[key] = value
+        } else {
+            this[key] = onConflict(this[key], value)
         }
     }
 }
@@ -20,15 +25,21 @@ fun <K, V> MutableMap<K, V>.smartMerge(other: Map<K, V>) {
  * Merge a Map into a MutableMap without overwriting existing keys
  *
  * @param other The map to merge
+ * @param onConflict Lambda to express how conflicts are resolved
  * @param convertIt Converter function to put Any to type 2
  *
  * @since 3.1.0
  * @author Thomas Obernosterer
  */
-fun <K, V> MutableMap<K, V>.smartMerge(other: Map<K, Any>, convertIt: (Any) -> V) {
+fun <K, V> MutableMap<K, V>.smartMerge(other: Map<K, Any>,
+                                       onConflict: (V?, V) -> V = { x, y -> x ?: y },
+                                       convertIt: (Any) -> V) {
     other.forEach { (key, value) ->
+        val newValue = convertIt(value)
         if (key !in this) {
-            this[key] = convertIt(value)
+            this[key] = newValue
+        } else {
+            this[key] = onConflict(this[key], newValue)
         }
     }
 }
@@ -38,17 +49,22 @@ fun <K, V> MutableMap<K, V>.smartMerge(other: Map<K, Any>, convertIt: (Any) -> V
  *
  * @param keys The Keys
  * @param values The Values
+ * @param onConflict Lambda to express how conflicts are resolved
  * @throws IncompatibleArrayLengthException when keys and values arrays have different sizes
  *
  * @since 3.0.0
+ * @since 5.1.0 - Throws exception when keys.size != values.size
  * @author Thomas Obernosterer
  */
 @Throws(IncompatibleArrayLengthException::class)
-fun <K, V> MutableMap<K, V>.mergeArrays(keys: Array<K>, values: Array<V>) {
+fun <K, V> MutableMap<K, V>.mergeArrays(keys: Array<K>, values: Array<V>,
+                                        onConflict: (V?, V) -> V = { x, y -> x ?: y}) {
     if (keys.size == values.size) {
         for (i in keys.indices) {
             if (keys[i] !in this) {
                 this[keys[i]] = values[i]
+            } else {
+                this[keys[i]] = onConflict(this[keys[i]], values[i])
             }
         }
     } else {
@@ -61,18 +77,25 @@ fun <K, V> MutableMap<K, V>.mergeArrays(keys: Array<K>, values: Array<V>) {
  *
  * @param keys The Keys
  * @param values The Values
+ * @param onConflict Lambda to express how conflicts are resolved
  * @param convertIt Converter function to put Any to type 2
  * @throws IncompatibleArrayLengthException when keys and values arrays have different sizes
  *
  * @since 3.1.0
+ * @since 5.1.0 - Throws exception when keys.size != values.size
  * @author Thomas Obernosterer
  */
 @Throws(IncompatibleArrayLengthException::class)
-fun <K, V> MutableMap<K, V>.mergeArrays(keys: Array<K>, values: Array<Any>, convertIt: (Any) -> V) {
+fun <K, V> MutableMap<K, V>.mergeArrays(keys: Array<K>, values: Array<Any>,
+                                        onConflict: (V?, V) -> V = { x, y -> x ?: y},
+                                        convertIt: (Any) -> V) {
     if (keys.size == values.size) {
         for (i in keys.indices) {
+            val newValue = convertIt(values[i])
             if (keys[i] !in this) {
-                this[keys[i]] = convertIt(values[i])
+                this[keys[i]] = newValue
+            } else {
+                this[keys[i]] = onConflict(this[keys[i]], newValue)
             }
         }
     } else {
---
+++ src/test/kotlin/klib/extensions/TMap.kt
@@ -0,0 +1,80 @@
+package klib.extensions
+
+import klib.exceptions.IncompatibleArrayLengthException
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class TMap {
+    @Test
+    fun `Test merging with Arrays`() {
+        val listA = mutableMapOf("a" to "b")
+        val arrayA = arrayOf("c", "e", "g")
+        val arrayB = arrayOf("d", "f", "h")
+
+        listA.mergeArrays(arrayA, arrayB)
+
+        assertEquals("{a=b, c=d, e=f, g=h}", listA.toString())
+    }
+
+
+    @Test
+    fun `Test merging with Arrays of different size`() {
+        val listA = mutableMapOf("a" to "b")
+        val arrayA = arrayOf("c", "e", "g", "i", "k")
+        val arrayB = arrayOf("d", "f", "h")
+
+        assertFailsWith(IncompatibleArrayLengthException::class) {
+            listA.mergeArrays(arrayA, arrayB)
+        }
+    }
+
+    @Test
+    fun `Test full merge`() {
+        val listA: MutableMap<String, String> = mutableMapOf(
+            "a" to "b",
+            "c" to "d"
+        )
+        val listB: MutableMap<String, String> = mutableMapOf(
+            "a" to "d",
+            "e" to "f",
+            "g" to "h"
+        )
+
+        listA.fullMerge(listB)
+
+        assertEquals("{a=d, c=d, e=f, g=h}", listA.toString())
+    }
+
+    @Test
+    fun `Test smart merge`() {
+        val listA: MutableMap<String, String> = mutableMapOf(
+            "a" to "b",
+            "c" to "d"
+        )
+        val listB: MutableMap<String, String> = mutableMapOf(
+            "a" to "d",
+            "e" to "f",
+            "g" to "h"
+        )
+
+        listA.smartMerge(listB)
+        assertEquals("{a=b, c=d, e=f, g=h}", listA.toString())
+    }
+
+    @Test
+    fun `Test smart merge with conversion`() {
+        val listA: MutableMap<String, String> = mutableMapOf(
+            "a" to "b",
+            "c" to "d"
+        )
+        val listB: MutableMap<String, Any> = mutableMapOf(
+            "a" to "d",
+            "e" to "f",
+            "g" to "h"
+        )
+
+        listA.smartMerge(listB) { it as String }
+        assertEquals("{a=b, c=d, e=f, g=h}", listA.toString())
+    }
+}
