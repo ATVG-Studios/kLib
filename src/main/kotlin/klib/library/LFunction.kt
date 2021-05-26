e0a7ed466af5cd197c36eebd9daa970616b58bde
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2021-05-26 20:51:35 +0200

    misc: add copyright +license notice to all files; update .idea files

--- src/main/kotlin/klib/library/LFunction.kt
+++ src/main/kotlin/klib/library/LFunction.kt
@@ -1,3 +1,13 @@
+/*
+ * This file belongs to the source code of: kLib
+ *
+ * Copyright© 2015-2021 Thomas Obernosterer, ATVG-Studios
+ * Copyright© 2019-2021 all kLib Contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package klib.library
 
 import java.lang.reflect.Method
